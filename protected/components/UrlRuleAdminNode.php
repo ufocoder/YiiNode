cf8eb213b9f9310763611e2aa7c8c9316e658a7b
Sergei Ivanov <xifrin@gmail.com>
2013-07-25 23:44:07 +0800

    Node url rules

---
+++ protected/components/UrlRuleAdminNode.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Admin node rule
+ *
+ * @author Sergei Ivanov <xifrin@gmail.com>
+ * @copyright 2013
+ *
+ * @version GIT: $Id$
+ * @revision: $Revision$
+ */
+class UrlRuleAdminNode extends CBaseUrlRule
+{
+    /**
+     * Create Url
+     *
+     * @param type $manager
+     * @param type $route
+     * @param type $params
+     * @param type $ampersand
+     * @return boolean
+     */
+    public function createUrl($manager,$route,$params,$ampersand)
+    {
+        if (empty($params['nodeId']) || empty($params['nodeAdmin']))
+            return false;
+
+        if (!empty($params['nodeId']))
+            $node = Yii::app()->getNodeByID($params['nodeId']);
+        else
+            return false;
+
+        if (!empty($node))
+            return 'admin/node/'.$node->id_node.'/'.$route;
+        else
+            return false;
+
+    }
+
+    /**
+     * Parse URL
+     *
+     * @param type $manager
+     * @param type $request
+     * @param type $pathInfo
+     * @param type $rawPathInfo
+     * @return boolean
+     * @throws CException
+     * @throws CHttpException
+     */
+    public function parseUrl($manager,$request,$pathInfo,$rawPathInfo)
+    {
+        preg_match('/^admin\/node\/(?<id>\d+)(\/(?<path>.*))?$/i', $pathInfo, $matches);
+        if (!empty($matches['id'])){
+            $node = Yii::app()->getNodeByID($matches['id']);
+            if (empty($node->id_node)){
+                if (YII_DEBUG)
+                    throw new CException(Yii::t('yii','Node ID #{id_node} not exists.', array('{id_node}' => $matches['id'])));
+                else
+                    throw new CHttpException(404, Yii::t('site', 'The requested page does not exist.'));
+            }
+            Yii::app()->setNode($node);
+            $path = !empty($matches['path'])?$matches['path']:'default';
+            return $node->module."/admin/".$path;
+        }
+        return false;
+    }
+
+}
---
+++ protected/components/UrlRuleModuleNode.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Module node rule
+ *
+ * @author Sergei Ivanov <xifrin@gmail.com>
+ * @copyright 2013
+ *
+ * @version GIT: $Id$
+ * @revision: $Revision$
+ */
+class UrlRuleModuleNode extends CBaseUrlRule
+{
+    /**
+     * Create Url
+     *
+     * @param type $manager
+     * @param type $route
+     * @param type $params
+     * @param type $ampersand
+     * @return boolean
+     */
+    public function createUrl($manager,$route,$params,$ampersand)
+    {
+        if (empty($params['nodeId']) && empty($params['nodePath']))
+            return false;
+
+        if (!empty($params['nodeId']))
+            $node = Yii::app()->getNodeByID($params['nodeId']);
+        elseif (!empty($params['nodePath']))
+            $node = Yii::app()->getNodeByPath($params['nodePath']);
+        else
+            return false;
+
+        if ($manager = Yii::app()->getModuleUrlManager(&$node)){
+            return ltrim($manager->createUrl($route, $params, $ampersand), "/");
+        }else
+            return false;
+    }
+
+    /**
+     * Parse URL
+     *
+     * @param type $manager
+     * @param type $request
+     * @param type $pathInfo
+     * @param type $rawPathInfo
+     * @return boolean
+     * @throws CException
+     * @throws CHttpException
+     */
+    public function parseUrl($manager,$request,$pathInfo,$rawPathInfo)
+    {
+        $path = Yii::app()->getRequest()->getPathInfo();
+
+        // ���� ����
+        $criteria = new CDbCriteria;
+        $criteria->order = 'path';
+        $criteria->addInCondition('path', Yii::app()->_getNodePathList($path));
+
+        $nodes = Node::model()->active()->findAll($criteria);
+        $node = end($nodes);
+
+        Yii::app()->setNode($node);
+
+        $chain = array();
+        foreach($nodes as $node)
+            $chain[$node->id_node] = $node;
+        Yii::app()->setNodeChain($chain);
+
+        if ($urlManager = Yii::app()->getModuleUrlManager(&$node))
+            return $urlManager->parseUrl($request);
+    }
+
+}
