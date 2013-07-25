f0f4df02e23eb5fa9f98208c297fa8daa19e6e83
Sergey Ivanov <xifrin@gmail.com>
2013-07-25 16:55:23 +0800

    update development config

--- protected/config/development.php
+++ protected/config/development.php
@@ -9,6 +9,18 @@ return CMap::mergeArray(
     require_once(dirname(__FILE__).'/main.php'),
     array(
         'components' => array(
+
+            'db'=> array(
+                'connectionString'      => 'mysql:host=localhost;dbname=YiiNode',
+                'enableProfiling'       => YII_DEBUG,
+                'enableParamLogging'    => YII_DEBUG,
+                'username'              => 'root',
+                'password'              => 'root',
+                'charset'               => 'utf8',
+                'tablePrefix'           => '',
+                'schemaCachingDuration' => 5000,
+            ),
+
             'log'=>array(
                 'class'=>'CLogRouter',
                 'enabled'=>YII_DEBUG,
@@ -18,7 +30,7 @@ return CMap::mergeArray(
                         'levels'    => 'error, warning, debug',
                     ),
                     array(
-                        'class'     => 'application.extensions.debug.YiiDebugToolbarRoute',
+                        'class'     => 'application.extensions.yii-debug-toolbar.YiiDebugToolbarRoute',
                         'ipFilters' => array('127.0.0.1', '195.66.89.207'),
                     ),
                 ),
