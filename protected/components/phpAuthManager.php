d1e1906aa395359bea99056a41b96e2503bd86a7
Sergey Ivanov <xifrin@gmail.com>
2013-08-03 21:29:17 +0800

    set default role and add role check

--- protected/components/phpAuthManager.php
+++ protected/components/phpAuthManager.php
@@ -7,18 +7,28 @@
  */
 class phpAuthManager extends CPhpAuthManager
 {
+    public $defautRoles = array(WebUser::ROLE_GUEST);
+
     /**
      * Auth manager initialize
-     */ 
+     */
     public function init()
     {
         if ($this->authFile===null)
             $this->authFile=Yii::getPathOfAlias('application.config.auth').'.php';
- 
+
         parent::init();
 
         // assign role
         if (!Yii::app()->user->isGuest)
-            $this->assign(Yii::app()->user->getRole(), Yii::app()->user->id);
+        {
+            $role = Yii::app()->user->getRole();
+            $roles = array_keys($this->getAuthItems(CAuthItem::TYPE_ROLE));
+
+            if (!empty($role) && in_array($role, $roles))
+                $this->assign($role, Yii::app()->user->id);
+        }
+
+
     }
 }
