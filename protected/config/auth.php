eebab6ae60e51db941cef509e806a9f58f2223d2
Sergey Ivanov <xifrin@gmail.com>
2013-08-03 21:30:04 +0800

    add role

--- protected/config/auth.php
+++ protected/config/auth.php
@@ -1,15 +1,29 @@
 <?php
 return array (
-    'administrator' => array (
+    'admin' => array (
         'type' => CAuthItem::TYPE_ROLE,
         'description' => '',
         'bizRule' => NULL,
         'data' => NULL,
         'children' => array(
             'moderator',
+            'manager',
+            'user',
         )
     ),
     'moderator' => array (
+        'type' => CAuthItem::TYPE_ROLE,
+        'description' => '',
+        'bizRule' => NULL,
+        'data' => NULL,
+    ),
+    'manager' => array (
+        'type' => CAuthItem::TYPE_ROLE,
+        'description' => '',
+        'bizRule' => NULL,
+        'data' => NULL,
+    ),
+    'user' => array (
         'type' => CAuthItem::TYPE_ROLE,
         'description' => '',
         'bizRule' => NULL,
