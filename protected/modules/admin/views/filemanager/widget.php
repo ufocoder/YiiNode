5f16bbc20e0f4caba71d3dd275b21a8bc506afad
Sergey Ivanov <xifrin@gmail.com>
2013-08-07 07:25:13 +0800

    change name of callback function

--- protected/modules/admin/views/filemanager/widget.php
+++ protected/modules/admin/views/filemanager/widget.php
@@ -3,7 +3,7 @@
     $this->widget('ext.elfinder2.ElFinderWidget', array(
         'connectorRoute' => 'admin/filemanager/connector',
         'settings' => array(
-            'editorCallback'=>'js:function(url) {
+            'getFileCallback'=>'js:function(url) {
                 var funcNum = window.location.search.replace(/^.*CKEditorFuncNum=(\d+).*$/, "$1");
                 window.opener.CKEDITOR.tools.callFunction(funcNum, url);
                 window.close();
