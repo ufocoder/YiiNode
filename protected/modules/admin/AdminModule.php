82451b6ed4f2afd5dc921ff1a9c4e84b0c9f957d
Sergey Ivanov <xifrin@gmail.com>
2013-08-10 22:17:41 +0800

    remove assets register

--- protected/modules/admin/AdminModule.php
+++ protected/modules/admin/AdminModule.php
@@ -30,11 +30,6 @@ class AdminModule extends WebModule
         ));
         $this->getComponent('bootstrap');
 
-        // assets
-        $assets = Yii::app()->assetManager->publish(dirname(__FILE__) . '/assets');
-        $cs = Yii::app()->getClientScript();
-        $cs->registerScriptFile($assets . '/admin.js');
-
         // redefine user url list
         Yii::app()->user->recoveryUrl = array('/admin/recovery');
         Yii::app()->user->loginUrl = array('/admin/login');
