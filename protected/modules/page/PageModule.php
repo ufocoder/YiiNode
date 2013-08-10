2f76a1396502f6320cc5204eb2a09326a6aeabfa
Sergey Ivanov <xifrin@gmail.com>
2013-08-10 20:20:57 +0800

    update 'page' module

--- protected/modules/page/PageModule.php
+++ protected/modules/page/PageModule.php
@@ -8,15 +8,45 @@
 class PageModule extends WebModule
 {
     /**
+     * Module initialize
+     */
+    public function init()
+    {
+        parent::init();
+    }
+
+    /**
+     * Returns the description of this module.
+     * @return string the description of this module.
+     */
+    public function getDescription()
+    {
+        return '';
+    }
+
+    /**
+     * Returns the version of this module.
+     *
+     * @return string the version of this module.
+     */
+    public function getVersion()
+    {
+        return '1.0';
+    }
+
+   /**
      * Правила маршрутизации
      */
     public function route()
     {
         return array(
-            '/' =>'page/default/index',
+            '/' => 'page/default/index',
         );
     }
 
+    /**
+     *
+     */
     public function onCreate($event)
     {
         $node = $event->sender;
