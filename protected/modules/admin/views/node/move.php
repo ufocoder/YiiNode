b0f609563f954f0c888d6280f9607aeeb01892c8
Sergey Ivanov <xifrin@gmail.com>
2013-08-11 20:32:33 +0800

    remove actions

--- protected/modules/admin/views/node/move.php
+++ protected/modules/admin/views/node/move.php
@@ -9,12 +9,6 @@
         Yii::t('site', 'Move')
     );
 
-    $this->actions = array(
-        array('label' => Yii::t('site', 'Node content'), 'url' => Yii::app()->createUrl('/admin/node/'.$nodeId), 'icon' => 'file'),
-        array('label' => Yii::t('site', 'Node settings'), 'url' => Yii::app()->createUrl('/admin/node/update', array('id'=>$nodeId)), 'icon' => 'pencil'),
-        array('label' => Yii::t('site', 'Node move'), 'url' => Yii::app()->createUrl('/admin/node/move', array('id'=>$nodeId)), 'icon' => 'move'),
-    );
-
     $this->renderPartial('/node/_move', array(
         'model' => $model,
         'nodes' => $nodes,
