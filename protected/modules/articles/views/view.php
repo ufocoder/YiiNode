d8c28878f7f8dbbad98ecc672b1bdcfb082f93a0
Sergey Ivanov <xifrin@gmail.com>
2013-10-03 13:38:37 +0800

    meta view

--- protected/modules/articles/views/view.php
+++ protected/modules/articles/views/view.php
@@ -1,4 +1,13 @@
 <?php
-	$this->title = $model->title;
+    $this->title = $model->title;
+
+    if (!empty($model->meta_title))
+        $this->pageTitle = $model->meta_title;
+
+    if (!empty($model->meta_description))
+        $this->pageDescription = $model->meta_description;
+
+    if (!empty($model->meta_keywords))
+        $this->pageKeywords = $model->meta_keywords;
 ?>
 <div><?php echo $model->content;?></div>
