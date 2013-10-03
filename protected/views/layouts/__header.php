17e35a86d63e386bcc738961c051219e0f9daec1
Sergey Ivanov <xifrin@gmail.com>
2013-10-03 13:38:43 +0800

    meta view

--- protected/views/layouts/__header.php
+++ protected/views/layouts/__header.php
@@ -1,12 +1,27 @@
 <?php
     $baseUrl = Yii::app()->request->baseUrl;
+
+    // meta
+    if (empty($this->title))
+        $this->title = Yii::app()->getNodeTitle();
+
+    if (empty($this->pageTitle))
+        $this->pageTitle = $this->title . " | " . Yii::app()->getSetting('sitename');
+
+    if (empty($this->pageDescription))
+        $this->pageDescription = Yii::app()->getNodeAttribute('meta_description');
+
+    if (empty($this->pageKeywords))
+        $this->pageKeywords = Yii::app()->getNodeAttribute('meta_keywords');
+
 ?><!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <title><?php echo CHtml::encode($this->pageTitle); ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
+    <meta name="keywords" content="<?php echo CHtml::encode($this->pageKeywords); ?>">
+    <meta name="description" content="<?php echo CHtml::encode($this->pageDescription); ?>">
     <meta name="author" content="">
     <!-- Le styles -->
     <link href="<?php echo $baseUrl; ?>/css/main.css" rel="stylesheet">
