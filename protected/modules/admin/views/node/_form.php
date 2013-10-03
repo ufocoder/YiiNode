6a9297f42d2688d0bc0085359bbe0224b68a51fc
Sergey Ivanov <xifrin@gmail.com>
2013-10-03 12:55:56 +0800

    meta

--- protected/modules/admin/views/node/_form.php
+++ protected/modules/admin/views/node/_form.php
@@ -28,92 +28,103 @@
 
 <?php echo $form->errorSummary($model); ?>
 
-<?php echo $form->textFieldRow($model, 'title', array('class'=>'span8')); ?>
-<?php if (!$model->isRoot() && (!empty($nodes) || !$model->isNewRecord)):
- // get Title html ID
-    $attribute = 'title';
-    $htmlOptions = array();
-    CHtml::resolveNameID($model, $attribute, $htmlOptions);
-    $titleFieldID = $htmlOptions['id'];
 
-    // get Slug html ID
-    $htmlOptions = array();
-    $attribute = 'slug';
-    CHtml::resolveNameID($model, $attribute, $htmlOptions);
-    $slugFieldID = $htmlOptions['id'];
+<fieldset>
+    <legend><?php echo Yii::t('site', 'Common information');?></legend>
+    <?php echo $form->textFieldRow($model, 'title', array('class'=>'span8')); ?>
+    <?php if (!$model->isRoot() && (!empty($nodes) || !$model->isNewRecord)):
+     // get Title html ID
+        $attribute = 'title';
+        $htmlOptions = array();
+        CHtml::resolveNameID($model, $attribute, $htmlOptions);
+        $titleFieldID = $htmlOptions['id'];
 
-    // register script
-    $script = "\$(document).ready(function(){\$('#" . $titleFieldID . "').syncTranslit({destination:'" . $slugFieldID . "'});});";
-    Yii::app()->getClientScript()->registerScript('article-form-slug', $script);
+        // get Slug html ID
+        $htmlOptions = array();
+        $attribute = 'slug';
+        CHtml::resolveNameID($model, $attribute, $htmlOptions);
+        $slugFieldID = $htmlOptions['id'];
 
-?>
-<?php echo $form->textFieldRow($model, 'slug', array('class'=>'span8')); ?>
-<?php endif; ?>
+        // register script
+        $script = "\$(document).ready(function(){\$('#" . $titleFieldID . "').syncTranslit({destination:'" . $slugFieldID . "'});});";
+        Yii::app()->getClientScript()->registerScript('article-form-slug', $script);
 
-<?php if ($model->isNewRecord): ?>
-<?php if (!empty($nodes)): ?>
-<div class="control-group">
-    <?php echo $form->labelEx($model, 'node_related', array('class'=>'control-label')); ?>
-    <div class="controls">
-        <?php $this->widget('bootstrap.widgets.TbSelect2',array(
-            'model' => $model,
-            'attribute' => 'node_related',
-            'asDropDownList' => true,
-            'options' => array(
-                'placeholder' => Yii::t("site", "Select a node"),
-                'width' => '370px',
-            ),
-            'val' => null,
-            'data' => $nodes,
-        )); ?>
+    ?>
+    <?php echo $form->textFieldRow($model, 'slug', array('class'=>'span8')); ?>
+    <?php endif; ?>
+
+    <?php if ($model->isNewRecord): ?>
+    <?php if (!empty($nodes)): ?>
+    <div class="control-group">
+        <?php echo $form->labelEx($model, 'node_related', array('class'=>'control-label')); ?>
+        <div class="controls">
+            <?php $this->widget('bootstrap.widgets.TbSelect2',array(
+                'model' => $model,
+                'attribute' => 'node_related',
+                'asDropDownList' => true,
+                'options' => array(
+                    'placeholder' => Yii::t("site", "Select a node"),
+                    'width' => '370px',
+                ),
+                'val' => null,
+                'data' => $nodes,
+            )); ?>
+        </div>
     </div>
-</div>
-<?php echo $form->radioButtonListRow($model, 'node_position', $model->values('position')); ?>
-<?php endif; ?>
+    <?php echo $form->radioButtonListRow($model, 'node_position', $model->values('position')); ?>
+    <?php endif; ?>
 
-<div class="control-group">
-    <?php echo $form->labelEx($model, 'module', array('class'=>'control-label')); ?>
-    <div class="controls">
-        <?php $this->widget('bootstrap.widgets.TbSelect2',array(
-            'model' => $model,
-            'attribute' => 'module',
-            'asDropDownList' => true,
-            'options' => array(
-                'placeholder' => Yii::t("site", "Select a module"),
-                'width' => '370px',
-            ),
-            'val' => null,
-            'data' => $modules,
-        ));
-        ?>
+    <div class="control-group">
+        <?php echo $form->labelEx($model, 'module', array('class'=>'control-label')); ?>
+        <div class="controls">
+            <?php $this->widget('bootstrap.widgets.TbSelect2',array(
+                'model' => $model,
+                'attribute' => 'module',
+                'asDropDownList' => true,
+                'options' => array(
+                    'placeholder' => Yii::t("site", "Select a module"),
+                    'width' => '370px',
+                ),
+                'val' => null,
+                'data' => $modules,
+            ));
+            ?>
+        </div>
     </div>
-</div>
-<?php endif; ?>
+    <?php endif; ?>
 
-<?php if (!empty($node_layouts)): ?>
-<div class="control-group">
-    <?php echo $form->labelEx($model, 'layout', array('class'=>'control-label')); ?>
-    <div class="controls">
-        <?php $this->widget('bootstrap.widgets.TbSelect2',array(
-            'model' => $model,
-            'attribute' => 'layout',
-            'asDropDownList' => true,
-            'options' => array(
-                'placeholder' => Yii::t("site", "Select a module"),
-                'width' => '370px',
-            ),
-            'val' => null,
-            'data' => $node_layouts,
-        ));
-        ?>
+    <?php if (!empty($node_layouts)): ?>
+    <div class="control-group">
+        <?php echo $form->labelEx($model, 'layout', array('class'=>'control-label')); ?>
+        <div class="controls">
+            <?php $this->widget('bootstrap.widgets.TbSelect2',array(
+                'model' => $model,
+                'attribute' => 'layout',
+                'asDropDownList' => true,
+                'options' => array(
+                    'placeholder' => Yii::t("site", "Select a module"),
+                    'width' => '370px',
+                ),
+                'val' => null,
+                'data' => $node_layouts,
+            ));
+            ?>
+        </div>
     </div>
-</div>
-<?php endif; ?>
+    <?php endif; ?>
 
-<?php echo $form->textAreaRow($model, 'description', array('class'=>'span8', 'rows'=>7, 'hint'=>Yii::t('site', 'Node description for administration or site manager'))); ?>
+    <?php echo $form->textAreaRow($model, 'description', array('class'=>'span8', 'rows'=>7, 'hint'=>Yii::t('site', 'Node description for administration or site manager'))); ?>
 
-<?php echo $form->checkBoxRow($model, 'enabled', $model->isNewRecord?array('checked'=>'checked'):array()); ?>
-<?php echo $form->checkBoxRow($model, 'hidden'); ?>
+    <?php echo $form->checkBoxRow($model, 'enabled', $model->isNewRecord?array('checked'=>'checked'):array()); ?>
+    <?php echo $form->checkBoxRow($model, 'hidden'); ?>
+</fieldset>
+
+<fieldset>
+    <legend><?php echo Yii::t('site', 'Meta information')?></legend>
+    <?php echo $form->textFieldRow($model, 'meta_title', array('class'=>'span8')); ?>
+    <?php echo $form->textAreaRow($model, 'meta_keywords', array('class'=>'span8', 'style'=>'min-height: 80px;')); ?>
+    <?php echo $form->textAreaRow($model, 'meta_description', array('class'=>'span8', 'style'=>'min-height: 80px;')); ?>
+</fieldset>
 
 <div class="form-actions">
     <?php $this->widget('bootstrap.widgets.TbButton', array('buttonType'=>'submit', 'type'=>'primary', 'label'=>($model->isNewRecord ? Yii::t('site', 'Create') : Yii::t('site', 'Save')))); ?>
