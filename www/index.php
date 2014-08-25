2cbfc45fbc2c62ce5e2977028d4bc1f55e6614bc
Sergey Ivanov <xifrin@gmail.com>
2014-08-25 16:26:22 +0800

    load thougth composer.json

--- www/index.php
+++ www/index.php
@@ -5,8 +5,13 @@ error_reporting(E_ALL);
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
 
+// autoload
+require dirname(__FILE__).'/../vendor/autoload.php';
+
+// path
+$yii = dirname(__FILE__) . '/../vendor/yiisoft/yii/framework/yiilite.php';
+
 // change the following paths if necessary
-$yii = dirname(__FILE__).'/../framework/yii.php';
 $config = dirname(__FILE__).'/../protected/config/development.php';
 
 require_once($yii);
