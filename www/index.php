80d127505c2a09b87d005f7268256ef1d70d1d5e
Sergey Ivanov <xifrin@gmail.com>
2014-08-25 17:27:37 +0800

    change path

--- www/index.php
+++ www/index.php
@@ -6,13 +6,12 @@ defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
 
 // autoload
-require dirname(__FILE__).'/../vendor/autoload.php';
-
-// path
-$yii = dirname(__FILE__) . '/../vendor/yiisoft/yii/framework/yiilite.php';
+require dirname(__FILE__).'/../protected/vendor/autoload.php';
 
 // change the following paths if necessary
 $config = dirname(__FILE__).'/../protected/config/development.php';
 
+// yii
+$yii = dirname(__FILE__) . '/../protected/vendor/yiisoft/yii/framework/yiilite.php';
 require_once($yii);
 Yii::createWebApplication($config)->run();
